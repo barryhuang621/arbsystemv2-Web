40e616b04962f2a2e8e8ab2d80275e128e308035
barryhuang621 <barryhuang621@gmail.com>
2026-08-01 16:59:54 +0800

    Enable polling mode in vite.config.js for network drive

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // 相容網路磁碟機 (NAS / Z:\ 磁碟) 的檔案監聽模式
+    }
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
